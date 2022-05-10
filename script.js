1e06298a9ac524d728d7d09f5dfe23a39b841b24
Ibrahim Sulemana <ibeevpersie@gmail.com>
2022-05-10 16:16:34 +0000

    update

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
-  $(".msgs").hide();
-  $(".submit-msg-btn").click(function() {
-    $(".msg-contents")
-      .transition({
-        animation: "fade down",
-        duration: 500
-      })
-      .transition({
-        animation: "fly left",
-        interval: 5000,
-        duration: 3000
-      });
-  });
-});
+// $(document).ready(function() {
+//   $(".msg-contents").hide();
+//   $(".contact__form").submit(function() {
+//     $(".msg-contents")
+//     .transition({
+//       animation: "fade down",
+//       interval: 3000,
+//       duration: 2000
+//     })
+//       .transition({
+//         animation: "fly left",
+//         interval: 5000,
+//         duration: 3000
+//       }).show();
+//   });
+// });
